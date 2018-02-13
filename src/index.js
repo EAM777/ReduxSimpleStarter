3b8f55457c76e85204aa5b9b68cda9430743abfe
Elliot <elliot@MacBook-Pro.local>
2018-02-13 00:15:36 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDb6PapmAT-mFatOC6HHc1kw0yEx5kvxGk';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component. This component should produce
+// some HTML
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>get high as fuck!</div>;
+}
+
+
+
+
+
+//  Take this component's generated HTML and put it
+// on the page (in the DOM)
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
